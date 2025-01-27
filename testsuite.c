acc2fc5d3387fe8e0bdb8f1b340c0cc4c0c848b6
IannigAndre <andre.i1@stjolorient.fr>
2025-01-27 14:27:04 +0100

    code du prof

--- testsuite.c
+++ testsuite.c
@@ -1,9 +1,11 @@
-/*************** Tests unitaires du module math ****************/
+/************* Tests unitaires - ligne de commande **************/
 #include <CUnit/CUnit.h>
 #include <CUnit/Basic.h>
-
-#include "math.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "erreur.h"
+#include "nombre.h"
 
 /** Montage de la fixation - appelé avant chaque cas de test. **/
 int init_suite(void) { return 0; }
@@ -11,22 +13,26 @@ int init_suite(void) { return 0; }
 /* Démontage de la fixation - appelé après chaque cas de test. */
 int clean_suite(void) { return 0; }
 
-/****** Cas de test - maximum avec arguments du même signe *****/
-void echo_valide(void) {
-  
+/******* Cas de test - lancement du client - cas nominal *******/
+/* Le binaire retourne 0 en cas de succès - exit(0)            */
+void test_ligne_commande_OK(void) {
+  /* La fonction system() exécute une commande depuis le shell */
+  CU_ASSERT(system("./multiplication 123 -5.1") == 0);
 }
 
-/**** Cas de test - maximum avec arguments de signes opposés ***/ 
-void test_maximum_signes_opposes(void) {
-  CU_ASSERT_EQUAL( maximum(-21, 21), 21);
-  CU_ASSERT_EQUAL( maximum(21, -21), 21);
-}
+/****** Cas de test - lancement du client - cas d'erreur *******/
+/* Le binaire retourne une valeur autre que 0 en cas d'échec   */
+void test_ligne_commande_erreur(void) {
+  /* Mauvais nombre d'argument                                 */
+  /* La fonction system() exécute une commande depuis le shell */
+  CU_ASSERT(system("./multiplication") != 0);
+  CU_ASSERT(system("./multiplication 1") != 0);
+  CU_ASSERT(system("./multiplication 1 2 3") != 0);
 
-/*** Cas de test - maximum avec arguments de valeurs égales ****/ 
-void test_maximum_egalite(void) {
-  CU_ASSERT_EQUAL( maximum(0, 0), 0);
-  CU_ASSERT_EQUAL( maximum(-100, -100), -100);
-  CU_ASSERT_EQUAL( maximum(1234, 1234), 1234);
+  /* Les arguments ne sont pas des nombres                     */
+  CU_ASSERT(system("./multiplication abc 123") != 0);
+  CU_ASSERT(system("./multiplication 123 abc") != 0);
+  CU_ASSERT(system("./multiplication abc abc") != 0);
 }
 
 /******************* Lancement des tests ***********************/
@@ -40,23 +46,21 @@ int main ( void )
       return CU_get_error();
 
    /* ajout de la suite de test */
-   pSuite = CU_add_suite( "Test module math", init_suite, clean_suite );
+   pSuite = CU_add_suite( "Test ligne de commande", init_suite, clean_suite );
    if ( NULL == pSuite ) {
       CU_cleanup_registry();
       return CU_get_error();
    }
 
    /* ajout des cas de test dans la suite de test */
-   if ( (NULL == CU_add_test(pSuite, "Cas de test - maximum avec arguments du même signe", test_maximum_meme_signe)) ||
-        (NULL == CU_add_test(pSuite, "Cas de test - maximum avec arguments de signes opposés", test_maximum_signes_opposes)) ||
-        (NULL == CU_add_test(pSuite, "Cas de test - maximum avec arguments égaux", test_maximum_egalite))
-      )
-   {;
+   if ((NULL == CU_add_test(pSuite, "Cas de test - cas nominal", test_ligne_commande_OK)) ||
+       (NULL == CU_add_test(pSuite, "Cas de test - ligne de commande sans argument", test_ligne_commande_erreur)))
+   {
       CU_cleanup_registry();
       return CU_get_error();
    }
 
-   /* lancement de tous les tests avac l'interface de base */
+   /* lancement de tous les tests avec l'interface de base */
    CU_basic_set_mode(CU_BRM_NORMAL);
    if ( CUE_SUCCESS != CU_basic_run_tests() )
       return CU_get_error();
@@ -66,5 +70,7 @@ int main ( void )
   
    /* cloture des tests */
    CU_cleanup_registry();
+
+   printf("\n");
    return status;
 }
