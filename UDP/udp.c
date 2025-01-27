5856d2f61934b0ef8dd2f0778ca649b9c1b7d86a
Charlotte CONTE <conte.c1@stjolorient.fr>
2025-01-27 13:57:58 +0100

    Ajout du code du professeur pour udp.c

--- UDP/udp.c
+++ UDP/udp.c
@@ -11,40 +11,38 @@
 
 /* Créer une socket */
 void creer_socket(char* adresseIP, int port, SOCK* sock) {
-	
-	sock->sockfd=socket(AF_INET, SOCK_DGRAM, 0) ;
-	
-	struct sockaddr_in addr;
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(2369);
-	inet_aton ("192.168.26.23", &addr.sin_addr);
-	
-	
-	if (sock == -1) {
-		printf(« Echec creation socket\n ») ;
-		exit(1) ;
-	}
+	sock->sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 	traiter_erreur(__FUNCTION__);
+
+	// adresse et port reutilisable
+	int optval = 1;
+    	setsockopt(sock->sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
+	traiter_erreur(__FUNCTION__);
+
 	/*A COMPLETER*/
 
+	if (strcmp(adresseIP, "") != 0)
+		sock->adresse.sin_addr.s_addr = inet_addr(adresseIP);
+	else 
+		sock->adresse.sin_addr.s_addr = INADDR_ANY;
 }
 
 /* Attacher une socket */
 void attacher_socket(SOCK* sock) {
 	
-	
+	bind(sock->sockfd,(struct sockaddr *)&sa, sizeof sa);
 	
 	traiter_erreur(__FUNCTION__);
 }
 
 /*Initialiser la structure adresse client */
 void init_addr(SOCK* sock) {
-	/*A COMPLETER*/
+	memset(&sock->adresse, 0, sizeof(sock->adresse));
 }
 
 /* Dimensionner la file d'attente d'une socket */
 void dimensionner_file_attente_socket(int taille, SOCK* sock) {
-	listen(sock->sockfd, taill);
+	listen(sock->sockfd, taille);
 	traiter_erreur(__FUNCTION__);
 }
 
@@ -61,6 +59,6 @@ void envoyer_message(SOCK* dst, char * message) {
 }
 /* Fermer la connexion */
 void fermer_connexion(SOCK* sock) {
-	/*A COMPLETER*/
+	close(sock->sockfd);
 	traiter_erreur(__FUNCTION__);
 }
